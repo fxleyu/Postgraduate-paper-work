e9ba26131ea5691b5ccb0578cd1c97bdcee4c334
fxleyu <fxleyu@csu.edu.cn>
2013-12-10 16:34:20 +0800

    fxleyu time20131210 message: 工作前的备份

--- src/Fx.java
+++ src/Fx.java
@@ -1,11 +1,7 @@
 
 public class Fx {
-
 	public static void main(String[] args) {
-		String fx = null;
-		fx +="fx";
-		System.out.println(fx);
-		System.out.println("Hello World!!!");
+		System.out.println("Hello World!!");
 	}
 
 }
