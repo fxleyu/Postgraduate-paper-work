ec78fc14fa70ce82603a3aada7cddb99530d6077
fxleyu <fxleyu@csu.edu.cn>
2013-12-26 10:26:49 +0800

    修改后 time 20131226 message 改

--- src/cn/csu/dianzi/LP/run/TestTimeCN.java
+++ src/cn/csu/dianzi/LP/run/TestTimeCN.java
@@ -3,15 +3,15 @@ package cn.csu.dianzi.LP.run;
 import java.util.ArrayList;
 
 import cn.csu.dianzi.LP.TimeCN;
-import cn.csu.dianzi.bean.Edge;
+import cn.csu.dianzi.bean.*;
 
 public class TestTimeCN extends TestCN{
 	public void run(double k){
-		ArrayList<Edge> A = select(-1);
-		ArrayList<Edge> C = select(0);
+		ArrayList<E> A = select(-1);
+		ArrayList<E> C = select(0);
 		add(C, A);
 		for(int i=1; i<14; i++){
-			ArrayList<Edge> nowList = select(i);
+			ArrayList<E> nowList = select(i);
 			TimeCN cn = new TimeCN(A, nowList, C);
 			System.out.println("第"+i+"次预测auc值为：" + cn.run(100000));
 			System.out.println("时间为(s)："+ cn.getTime());
--- src/cn/csu/dianzi/bean/E.java
+++ src/cn/csu/dianzi/bean/E.java
@@ -1,17 +1,17 @@
 package cn.csu.dianzi.bean;
 
-public class Edge {
+public class E {
 	public int a;
 	public int b;
 	
-	public Edge(int a, int b){
+	public E(int a, int b){
 		this.a = a;
 		this.b = b;
 	}
 	
 	public boolean equals(Object o){
-		if(o instanceof Edge){
-			Edge temp = (Edge)o;
+		if(o instanceof E){
+			E temp = (E)o;
 			if(temp.a == this.a && temp.b == this.b ){
 				return true;
 			}else if(temp.a == this.b && temp.b == this.a){
